a39adef46080c0f23e8d667f15bdbfb2d58f6cd5
yootina <tinayoo1004@gmail.com>
2023-10-18 21:59:07 +0900

    수정

--- programmers/가위바위보/sol.py
+++ programmers/가위바위보/sol.py
@@ -15,9 +15,35 @@ print(solution("205"))
 
 ######################################
 
+# def solution(rsp):
+#     answer = ''
+
+#     for char in rsp:
+#         if char == '2':
+#             answer += '0'
+#         elif char == '0':
+#             answer += '5'
+#         else:
+#             answer += '2'
+
+#     return answer
+
+
 def solution(rsp):
     answer = ''
 
+    rsp_dict = {
+        '2': '0',
+        '0': '5',
+        '5': '2',
+    }
+
     for char in rsp:
-        if
-        return answer
+        answer += rsp_dict[char]
+
+    return answer
+
+
+
+print(solution("2"))
+print(solution("205"))
